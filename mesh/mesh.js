116ba7deebe2e1f48b85326ce3cf239c5cf292f3
Raj Judge <43121310+judgeraj@users.noreply.github.com>
2024-01-25 19:36:17 -0800

    Add files via upload

---
+++ mesh/mesh.js
@@ -0,0 +1,101 @@
+
+const VERTEX_STRIDE = 28;
+
+class Mesh {
+    /** 
+     * Creates a new mesh and loads it into video memory.
+     * 
+     * @param {WebGLRenderingContext} gl  
+     * @param {number} program
+     * @param {number[]} vertices
+     * @param {number[]} indices
+    */
+    constructor( gl, program, vertices, indices ) {
+        this.verts = create_and_load_vertex_buffer( gl, vertices, gl.STATIC_DRAW );
+        this.indis = create_and_load_elements_buffer( gl, indices, gl.STATIC_DRAW );
+
+        this.n_verts = vertices.length;
+        this.n_indis = indices.length;
+        this.program = program;
+    }
+
+    /**
+     * Create a box mesh with the given dimensions and colors.
+     * @param {WebGLRenderingContext} gl 
+     * @param {number} width 
+     * @param {number} height 
+     * @param {number} depth 
+     */
+
+    static box( gl, program, width, height, depth ) {
+        let hwidth = width / 2.0;
+        let hheight = height / 2.0;
+        let hdepth = depth / 2.0;
+
+        let verts = [
+            hwidth, -hheight, -hdepth,      1.0, 0.0, 0.0, 1.0,
+            -hwidth, -hheight, -hdepth,     0.0, 1.0, 0.0, 1.0,
+            -hwidth, hheight, -hdepth,      0.0, 0.0, 1.0, 1.0,
+            hwidth, hheight, -hdepth,       1.0, 1.0, 0.0, 1.0,
+
+            hwidth, -hheight, hdepth,       1.0, 0.0, 1.0, 1.0,
+            -hwidth, -hheight, hdepth,      0.0, 1.0, 1.0, 1.0,
+            -hwidth, hheight, hdepth,       0.5, 0.5, 1.0, 1.0,
+            hwidth, hheight, hdepth,        1.0, 1.0, 0.5, 1.0,
+        ];
+
+        let indis = [
+            // clockwise winding
+            /*
+            0, 1, 2, 2, 3, 0, 
+            4, 0, 3, 3, 7, 4, 
+            5, 4, 7, 7, 6, 5, 
+            1, 5, 6, 6, 2, 1,
+            3, 2, 6, 6, 7, 3,
+            4, 5, 1, 1, 0, 4,
+            */
+
+            // counter-clockwise winding
+            0, 3, 2, 2, 1, 0,
+            4, 7, 3, 3, 0, 4,
+            5, 6, 7, 7, 4, 5,
+            1, 2, 6, 6, 5, 1,
+            3, 7, 6, 6, 2, 3,
+            4, 0, 1, 1, 5, 4,
+        ];
+
+        return new Mesh( gl, program, verts, indis );
+    }
+
+
+    /**
+     * Render the mesh. Does NOT preserve array/index buffer or program bindings! 
+     * 
+     * @param {WebGLRenderingContext} gl 
+     */
+    render( gl ) {
+        gl.cullFace( gl.BACK );
+        gl.enable( gl.CULL_FACE );
+        
+        gl.useProgram( this.program );
+        gl.bindBuffer( gl.ARRAY_BUFFER, this.verts );
+        gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.indis );
+
+        set_vertex_attrib_to_buffer( 
+            gl, this.program, 
+            "coordinates", 
+            this.verts, 3, 
+            gl.FLOAT, false, VERTEX_STRIDE, 0 
+        );
+
+        set_vertex_attrib_to_buffer( 
+            gl, this.program, 
+            "color", 
+            this.verts, 4, 
+            gl.FLOAT, false, VERTEX_STRIDE, 12
+        );
+
+        gl.drawElements( gl.TRIANGLES, this.n_indis, gl.UNSIGNED_SHORT, 0 );
+    }
+
+}
